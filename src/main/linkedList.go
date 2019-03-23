28833589bbc68c92e4f68924e638b4979bee9bcf
matkateresa <attwistingnether@gmail.com>
2019-03-23 23:49:16 +0100

    refactoring

--- src/main/linkedList.go
+++ src/main/linkedList.go
@@ -5,36 +5,45 @@ import (
 	"sync"
 )
 
+type list struct {
+	start *item
+}
+
 type item struct {
 	value int64
 	next  *item
 }
 
-func printList(list *item) {
+func printList(list *list) {
 	i := 0
-	for item := list; item != nil; item = item.next {
+	for item := list.start; item != nil; item = item.next {
 		fmt.Printf("[%v] -> %v\n", i, item.value)
 		i++
 	}
 }
 
-func addLast(list, newItem *item) *item {
-	if list == nil {
-		return list
+func (l *list) addLast(item *item) {
+	if l.start == nil {
+		l.start = item
+		return
 	}
 
-	for item := list; item != nil; item = item.next {
-		if item.next == nil {
-			item.next = newItem
-			return list
+	for i := l.start; i != nil; i = i.next {
+		if i.next == nil {
+			i.next = item
+			return
 		}
 	}
-	return list
 }
 
-func addFirst(list, item *item) *item {
-	item.next = list
-	return item
+func (l *list) addFirst(item *item) {
+	if l.start == nil {
+		l.start = item
+	} else {
+		item.next = l.start
+		l.start = item
+	}
+
 }
 
 func delete(list, toDelete *item) {
--- src/main/main.go
+++ src/main/main.go
@@ -1,16 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	a := &item{1, nil}
-	c := &item{3, a}
-	b := &item{2, c}
 
-	list := b
-	list = addLast(list, &item{4, nil})
-	list = addFirst(list, &item{55, nil})
-	fmt.Println(get(list, 2))
-	deleteAtIndex(list, 2)
-	printList(list)
+	linkedList := &list{nil}
+	linkedList.addFirst(&item{2, nil})
+	linkedList.addFirst(&item{44, nil})
+	linkedList.addLast(&item{55, nil})
+	printList(linkedList)
 }
